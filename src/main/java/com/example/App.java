3ae6729332e9b00e40ee93498e2a2d2ca34f1903
AahanShetye <aahanshetye@gmail.com>
2025-08-31 19:23:09 +0530

    make some changes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Maven CI!");
+        System.out.println("Some changes");
     }
 
     public int add(int a, int b) {
